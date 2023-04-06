b98ce50f1aae01befcb609e75c530ee8a8df6fef
AlexSanako <116287214+AlexSanako@users.noreply.github.com>
2023-04-06 16:30:16 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src = '${preview}' alt='${description}' />
+   </a>
+</li>`;
+});
+
+galleryList.insertAdjacentHTML('beforeend', markup.join(''));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
